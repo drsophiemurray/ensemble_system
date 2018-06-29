220d19c54c8cacad47cd305afc768c50a3e5ec1f
Sophie Murray <sophiemurray@gmail.com>
2018-06-29 10:55:30 +0100

    function to get forecasts
    added function that will get yesterday (for verification) and today (for forecast) data. at the moment it just prints out the results, todo create a database to store them

--- get_forecasts.py
+++ get_forecasts.py
@@ -4,22 +4,28 @@ In ideal world FLARECAST would work, but nope so will use Scoreboard
 Just get whatever raw data there is for M and X flares
 -most recent forecast since last run?
 
+
 Steps:
 - get current time
 - grab forecasts for each iswa product
 - put in database
 - next codes: average, verify yesterday
+
+Note:
+- UFCORIN has not been included as too different to combine
+- SIDC has not been included as issued at midday for 12 hours so cannot combine easily
 """
 
 import requests
 import datetime
 
-iswa_fulldisk_forecast_products = {"ASSA_1_FULLDISK", "ASSA_24H_1_FULLDISK",
-                                    "AMOS_v1_FULLDISK", "BoM_flare1_FULLDISK",
-                                    "MO_TOT1_FULLDISK", "NOAA_1_FULLDISK",
-                                    "SIDC_Operator_FULLDISK"}
-m_plus_forecasts = ["AMOS_v1_FULLDISK","BoM_flare1_FULLDISK", "SIDC_Operator_FULLDISK"]
-iswa_data_link = "https://iswa.gsfc.nasa.gov/IswaSystemWebApp/flarescoreboard/hapi/data"
+ISWA_FULLDISK_PRODUCTS = {"ASSA_1_FULLDISK", "ASSA_24H_1_FULLDISK",
+                          "AMOS_v1_FULLDISK", "BoM_flare1_FULLDISK",
+                          "MO_TOT1_FULLDISK", "NOAA_1_FULLDISK",
+                          "SIDC_Operator_FULLDISK", "UFCORIN_1_FULLDISK"}
+M_PLUS_FORECASTS = ["AMOS_v1_FULLDISK","BoM_flare1_FULLDISK",
+                    "SIDC_Operator_FULLDISK", "UFCORIN_1_FULLDISK"]
+ISWA_DATA_LINK = "https://iswa.gsfc.nasa.gov/IswaSystemWebApp/flarescoreboard/hapi/data"
 
 def main():
     """
@@ -27,36 +33,46 @@ def main():
     For ISWA, values are in 'data' and descriptions are in 'parameters'. No data is '-1'.
     """
     time_now = datetime.datetime.utcnow()
+    #TODO always take midnight? means dropping SIDC
     time_start = (time_now-datetime.timedelta(days=1)).strftime('%Y-%m-%dT%H:%M:%S.%f')
     time_end = time_now.strftime('%Y-%m-%dT%H:%M:%S.%f')
-    for product in iswa_fulldisk_forecast_products:
+    for product in ISWA_FULLDISK_PRODUCTS:
+        #TODO remove hardcoded date when all available and replace with time_start and time_end for realtime
         selection = {"id":product,
-                     "time.min":'2016-09-05T00:00:00.0',
-                     "time.max":'2016-09-07T00:00:00.0',
+                     "time.min":'2016-09-04T23:50:00.0',
+                     "time.max":'2016-09-06T00:11:00.0',
                      "format":"json",
                      "options":"fields.all"}
-        response = requests.get(iswa_data_link, params=selection)
+        response = requests.get(ISWA_DATA_LINK , params=selection)
         if response.status_code == 200:
             data = response.json()
             if data['data']:
-                forecast_time = data['data'][0][0]
-                if any(product==forecast for forecast in m_plus_forecasts):
-                    m_prob = data['data'][0][6]
-                else:
-                    m_prob = data['data'][0][4]
-                x_prob = data['data'][0][7]
-                print(product,forecast_time,m_prob,x_prob)
+                #first yesterdays forecast for verification
+                get_iswa_forecasts(product, data, day=0)
+                #now today for the ensemble forecast
+                get_iswa_forecasts(product, data, day=len(data['data'])-1)
 
 
-
+def get_iswa_forecasts(product, data, day):
+    """
+    Grab M and X forecasts for particular date
+    product: The forecast product being grabbed
+    data: Forecast data taken from API
+    day: Time of interest
+    :return: TODO create a pandas database
+    """
+    forecast_time = data['data'][day][0]
+    if any(product == forecast for forecast in M_PLUS_FORECASTS):
+        m_prob = data['data'][day][6]
+    else:
+        m_prob = data['data'][day][4]
+    x_prob = data['data'][day][7]
+    print(product, forecast_time, m_prob, x_prob)
+    return
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
 
 
 
