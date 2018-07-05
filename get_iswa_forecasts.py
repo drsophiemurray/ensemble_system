10619b77c75537878045dc09dafaef22e3d130ea
Sophie Murray <sophiemurray@gmail.com>
2018-07-05 15:49:48 +0100

    tidy
    just a bit of documentation tidying up

--- get_iswa_forecasts.py
+++ get_iswa_forecasts.py
@@ -13,6 +13,37 @@ Steps:
 Note:
 - UFCORIN has not been included as too different to combine
 - SIDC has not been included as issued at midday for 12 hours so cannot combine easily
+- Below code is equivalent of something like:
+    https://iswa.gsfc.nasa.gov/IswaSystemWebApp/flarescoreboard/hapi/data?id=NOAA_1_FULLDISK&time.min=2017-09-05T00:00:00.0&time.max=2017-09-10T00:00:00.0&format=json&options=fields.all
+- All data fields are:
+    # 0: start_window
+    # 1: end_window
+    # 2: issue_time
+    # 3: C
+    # 4: M
+    # 5: CPlus
+    # 6: MPlus
+    # 7: X
+    # 8: C_uncertainty
+    # 9: M_uncertainty
+    # 10: CPlus_uncertainty
+    # 11: MPlus_uncertainty
+    # 12: X_uncertainty
+    # 13: C_value_lower
+    # 14: M_value_lower
+    # 15: CPlus_value_lower
+    # 16: MPlus_value_lower
+    # 17: X_value_lower
+    # 18: C_value_higher
+    # 19: M_value_higher
+    # 20: CPlus_value_higher
+    # 21: MPlus_value_higher
+    # 22: X_value_higher
+    # 23: C_level
+    # 24: M_level
+    # 25: CPlus_level
+    # 26: MPlus_level
+    # 27: X_level
 """
 
 import requests
@@ -51,6 +82,8 @@ def main():
 
         response = requests.get(ISWA_DATA_LINK ,
                                 params=selection)
+#        print(response.content)
+
         if response.status_code == 200:
             data = response.json()
             if data['data']:
@@ -100,52 +133,4 @@ def grab_forecasts(product, data, day):
 
 if __name__ == '__main__':
     main()
-
-
-
-#Notes
-#get rid of u: encode('ascii', 'ignore') or just str()
-#or:
-#str(data['data'][0][0])
-#or:
-#>>> import json, ast
-#>>> r = {u'name': u'A', u'primary_key': 1}
-#>>> ast.literal_eval(json.dumps(r))
-#{'name': 'A', 'primary_key': 1}
-
-# equivalent of https://iswa.gsfc.nasa.gov/IswaSystemWebApp/flarescoreboard/hapi/data?id=NOAA_1_FULLDISK&time.min=2017-09-05T00:00:00.0&time.max=2017-09-10T00:00:00.0&format=json&options=fields.all
-# fields.all:
-# 0: start_window
-# 1: end_window
-# 2: issue_time
-# 3: C
-# 4: M
-# 5: CPlus
-# 6: MPlus
-# 7: X
-# 8: C_uncertainty
-# 9: M_uncertainty
-# 10: CPlus_uncertainty
-# 11: MPlus_uncertainty
-# 12: X_uncertainty
-# 13: C_value_lower
-# 14: M_value_lower
-# 15: CPlus_value_lower
-# 16: MPlus_value_lower
-# 17: X_value_lower
-# 18: C_value_higher
-# 19: M_value_higher
-# 20: CPlus_value_higher
-# 21: MPlus_value_higher
-# 22: X_value_higher
-# 23: C_level
-# 24: M_level
-# 25: CPlus_level
-# 26: MPlus_level
-# 27: X_level
-
-#    print(response.content)
-#    data['data']
-#    data['parameters']
-
 
