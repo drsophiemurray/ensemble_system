8911d9eb11c334c7f1e226a54ba4ecd75744e30e
Sophie Murray <sophiemurray@gmail.com>
2018-07-05 14:50:42 +0100

    fcast tutorial
    copied over whats in the flarecast api jupyter notebook (basically, not much :-1: )

--- get_flarecast_forecasts.py
+++ get_flarecast_forecasts.py
@@ -2,16 +2,16 @@
 Testing pulling in all the FLARECAST ML forecasts
 (even though real time currently doesnt work)
 """
-
+import requests
 
 def main():
-
-
-
-
-
-
-
+    prediction_service = "api.flarecast.eu/prediction"
+    fc_id = "prediction-00000000-0000-0000-0000-000000290b20"
+    url = "http://%s/prediction/data?prediction_fc_id=%s" % (prediction_service, fc_id)
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+    print(data)
 
 if __name__ == '__main__':
     main()
