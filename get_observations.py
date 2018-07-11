73466e732efe77443acf04ae7c47d03c2e68bbad
Sophie Murray <sophiemurray@gmail.com>
2018-07-11 13:41:07 +0100

    download any date
    updated to instead download yesterdays flare events, instead download a date specificied by user in call to code.

--- get_observations.py
+++ get_observations.py
@@ -10,22 +10,26 @@ anytime ftp://ftp.swpc.noaa.gov/pub/indices/events/20180711events.txt
 
 
 import datetime
+import os
 
 def main(date):
     """
     Get flare events for a particular date
+    date should be a string in format 'YYYYMMDD'
     """
+    file = date + 'events.txt'
     ftp = ftplib.FTP('ftp.swpc.noaa.gov')
     ftp.login()
     ftp.cwd('pub/indices/events/')
-    ftp.retrbinary('RETR yesterday.txt', open(cwd + '/yesterday.txt', "wb").write)
+    ftp.retrbinary('RETR '+file, open(os.getcwd() + '/' + file, "wb").write)
     ftp.close()
 
     flare_list = []
-    with open('yesterday.txt', "r") as inp:
+    with open(file, "r") as inp:
         for line in inp:
             if "XRA" in line:
                 flare_list.append(line[0:80])
+    os.remove(file)
 
 
 
