24a1810e4dd3a0e12c17270a33be0c50ba11257c
Sophie Murray <sophiemurray@gmail.com>
2018-08-24 09:17:24 +0100

    system plans
    added some ideas for what to call in other parts of system not yet created. also added keyword to get_observations so will load test set from 2016.

--- ensemble_system.py
+++ ensemble_system.py
@@ -1,5 +1,7 @@
 """
-This will be the main wrapper
+This will be the main wrapper.
+Each code self-contained and doesnt have to be used with the ensemble as long as in correct data format.
+All in pandas databases.
 """
 import get_observations
 import get_iswa_forecasts
@@ -7,17 +9,33 @@ import numpy as np
 
 def main():
     """
-    Grab forecasts, create an ensemble, verify
+    Grab forecasts.
+    Create an average ensemble, then weighted ensemble using defined properties,
+    Grab latest data.
+    Verify.
     """
+    # Grab forecasts
     iswa_yday, iswa_today = get_iswa_forecasts.main()
-    # Get average
-    average_iswa_today_m = np.average(iswa_today['x_prob'])
-    average_iswa_today_x = np.average(iswa_today['m_prob'])
-    # Get weighted ensemble
+    print(iswa_today)
 
-    # Verify yesterdays forecast
-    # First get observations
-    obs_list = get_observations()
+    # Get average
+    average_iswa_today_m = np.average(iswa_today['m_prob'])
+    average_iswa_today_x = np.average(iswa_today['x_prob'])
+
+    # Get weighted ensemble
+#    ensemble_out = ensemble(iswa_today,
+#                            type='probabilistic',
+#                            metric='brier',
+#                            uncertainty=True)
+    # Write out to file
+
+    # Get observations
+    obs_list = get_observations.main('20160723')
+    print(obs_list)
+
+    # Run through verification
+#    verify(forecasts=iswa_yday, events=obs_list,
+#           visual=True)
 
 
 if __name__ == '__main__':
